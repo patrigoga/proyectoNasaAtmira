1e1ab10030784b81204f64bcf0d9e3654235118e
Patricio Gómez García <patrigoga@gmail.com>
2022-02-12 13:16:34 +0100

    creacion de proyecto y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { DetalleComponent } from './componentes/detalle/detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',pathMatch:'full',redirectTo:'dashboard'},
+
+  {path:'dashboard', component:DashboardComponent},
+  {path:'detalles', component:DetalleComponent},
+  {path:'**',redirectTo:'dashboard'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { DetalleComponent } from './componentes/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetalleComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
